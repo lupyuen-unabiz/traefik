bbbcb23b0805d785ccea82784d05f440ad2e81be
lupyuen-unabiz <ly.lee@unabiz.comly.lee@unabiz.com>
2018-03-22 23:35:38 +0800

    Added deployment scripts

---
+++ scripts/appdeploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# Deploy the web app to Google App Engine.
+
+# sudo apt install golang
+# sudo apt install google-cloud-sdk-app-engine-go
+# echo 'export GOROOT=$HOME/go' >>~/.zshrc
+# echo 'export PATH=$PATH:$GOROOT/bin' >>~/.zshrc
+# echo 'export GOPATH=$GOROOT/bin' >>~/.zshrc
+# go get -u google.golang.org/appengine
+
+# Set GCLOUD_PROJECT in .env
+export GCLOUD_PROJECT=
+. .env
+
+# Deploy to Google Cloud.
+gcloud config set project ${GCLOUD_PROJECT}
+gcloud config list project
+
+echo ========= ${name} ========= gcloud app deploy app.yaml --quiet --verbosity=info
+gcloud app deploy app.yaml --quiet --verbosity=info
+
+echo ========= ${name} Deployed! =========
