e6a81d93b7df11a07df79d9f768494f3cf8e231d
lupyuen-unabiz <ly.lee@unabiz.com>
2018-04-01 14:06:49 +0800

    Splitting test client requests

--- cmd/traefik/scripts/appdeploy.sh
+++ cmd/traefik/scripts/appdeploy.sh
@@ -1,25 +1,49 @@
 #!/usr/bin/env bash
 # Deploy the web app to Google App Engine.
 
-# sudo apt install golang
-# sudo apt install google-cloud-sdk-app-engine-go
-# echo 'export GOROOT=$HOME/go' >>~/.zshrc
-# echo 'export PATH=$PATH:$GOROOT/bin' >>~/.zshrc
-# echo 'export GOPATH=$GOROOT/bin' >>~/.zshrc
-# go get -u google.golang.org/appengine
+name=traefik
+package=github.com/containous/traefik
+
+OLD_GOPATH=${GOPATH}
+export GOPATH=/tmp/gopath_${name}
+
+echo GOROOT=${GOROOT}
+echo GOPATH=${GOPATH}
+echo OLD_GOPATH=${OLD_GOPATH}
+
+echo ========= ${name} ========= "rm -rf ${GOPATH}/src/*"
+rm -rf ${GOPATH}/src/*
+
+echo ========= ${name} ========= "mkdir -p ${GOPATH}/src/${package}/"
+mkdir -p ${GOPATH}/src/${package}
+
+echo ========= ${name} ========= "cp -r ${OLD_GOPATH}/src/${package}/vendor/* ${GOPATH}/src/"
+cp -r ${OLD_GOPATH}/src/${package}/vendor/* ${GOPATH}/src/
+
+echo ========= ${name} ========= "cp -r ${OLD_GOPATH}/src/${package}/* ${GOPATH}/src/${package}/"
+cp -r ${OLD_GOPATH}/src/${package}/* ${GOPATH}/src/${package}/
+
+echo ========= ${name} ========= "rm -rf ${GOPATH}/src/${package}/vendor/"
+rm -rf ${GOPATH}/src/${package}/vendor/
+
+echo ========= ${name} ========= "./scripts/go-get.sh"
+./scripts/go-get.sh
 
 # Set GCLOUD_PROJECT in .env
 export GCLOUD_PROJECT=
 . ../../.env
 
-echo GOROOT=${GOROOT}
-echo GOPATH=${GOPATH}
-
 # Deploy to Google Cloud.
+echo ========= ${name} ========= gcloud config set project ${GCLOUD_PROJECT}
 gcloud config set project ${GCLOUD_PROJECT}
+
+echo ========= ${name} ========= gcloud config list project
 gcloud config list project
 
 echo ========= ${name} ========= gcloud app deploy app.yaml --quiet --verbosity=info
 gcloud app deploy app.yaml --quiet --verbosity=info
 
 echo ========= ${name} Deployed! =========
+
+export GOPATH=${OLD_GOPATH}
+echo GOPATH=${GOPATH}
